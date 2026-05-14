8ce6000382a1b25272071388cfbdf26ad59e9b56
AleCanzo <alessandro.canzoneri00@gmail.com>
2026-05-14 17:30:10 +0200

    .

--- 070. Totient Permutation.py
+++ 070. Totient Permutation.py
@@ -1,19 +1,19 @@
 from sympy.functions.combinatorial.numbers import totient
 
-min = 10
-
 def is_permutation(n1, n2):
     s1, s2 = str(n1), str(n2)
     if len(s1) != len(s2):
         return False
     return sorted(s1) == sorted(s2)
 
-for n in range(3, 10_000_001):
-    phi = totient(n)
-    if is_permutation(phi, n):
-        N = float(n/phi)
-        if N < min:
-            min = N
-            n_min = n
-        
-print(n_min)
+if __name__ == "__main__":
+    min = 10
+    for n in range(3, 10_000_001):
+        phi = totient(n)
+        if is_permutation(phi, n):
+            N = float(n/phi)
+            if N < min:
+                min = N
+                n_min = n
+            
+    print(n_min)
