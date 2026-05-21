8b626d0c002e981f644e7d1a0a78d8e2fa782d99
AleCanzo <alessandro.canzoneri00@gmail.com>
2026-05-21 17:46:04 +0200

    .

--- 071. Ordered Fractions.py
+++ 071. Ordered Fractions.py
@@ -1,13 +1,17 @@
 from fractions import Fraction
 import matplotlib.pyplot as plt
 
-l = []
-for d in range(1, 1_000_000):        
-    for n in range(1, d):
-        x = Fraction(n, d)
-        l.append(x)
-        #print(n, d, x)
-l = list(dict.fromkeys(l))
+for m in range(7, 1_000):
+    l = []
+    for d in range(1, m):        
+        for n in range(1, d):
+            x = Fraction(n, d)
+            l.append(x)
+            #print(n, d, x)
+    l = list(dict.fromkeys(l))
+    #print(l)
+    min = l[l.index(Fraction(3, 5)) - 1]
+    
+    print(l[l.index(Fraction(3, 5)) - 1])
+    l.sort()
 
-l.sort()
-print(l)
